99c51e1964d831f080a03409b6eded1106156dc2
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-07-13 20:02:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string greeting = @"Welcome to Bleuprint!
+The #1 Digital Marketplace!";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Enter Your Product!");
+
+string response = Console.ReadLine().Trim();
+
+while (string.IsNullOrWhiteSpace(response))
+{
+    Console.WriteLine("Try Again! Please Enter A Response!");
+    response = Console.ReadLine().Trim();
+}
+    Console.WriteLine($"You chose: {response}");
